583e405ef210295cdd1557a624291c01f6dd1edc
Lennart Melzer <me@lmaa.name>
2012-04-02 11:48:06 +0200

    [add] resolve, prepare and link

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 var _ = require('underscore')._,
-    _s = require('underscore.string');
+    _s = require('underscore.string'),
+    mkdirp = require('mkdirp'),
+    path = require('path'),
+    fs = require('fs');
 
 function Partition(options) {
   options = options || {};
 
-  this.root = options.root;
+  this.root = path.resolve(process.cwd(), options.root);
 }
+
+/*
+ * Synchronous API Methods
+ */
 
 Partition.prototype.scheme = function(id) {
   if(_.isNumber(id)) {
@@ -16,6 +23,52 @@ Partition.prototype.scheme = function(id) {
   }
   else {
     return null;
+  }
+};
+
+Partition.prototype.resolve = function(target) {
+  if(target) {
+    return path.resolve(this.root, target.toString());
+  }
+  else {
+    return this.root;
+  }
+};
+
+/*
+ * Asynchronous API Methods
+ */
+
+Partition.prototype.prepare = function(target, cb) {
+  var parent = path.dirname(target);
+  path.exists(parent, function(exists) {
+    if(exists) {
+      cb(null);
+    }
+    else {
+      mkdirp(parent, function(err) {
+        cb(err);
+      });
+    }
+  }); 
+};
+
+Partition.prototype.link = function(id, cb) {
+  var src = this.resolve(id),
+      target = this.resolve(this.scheme(id));
+
+  if(path.existsSync(src)) {
+    this.prepare(target, function(err) {
+      if(err) {
+        cb(err);
+      }
+      else {
+        fs.link(src, target, cb);
+      }
+    });
+  }
+  else {
+    cb("missing source folder '" + src + "'");
   }
 };
 
--- test/test.partition.js
+++ test/test.partition.js
@@ -15,56 +15,193 @@ describe("Partition", function(){
 
       expect(partition).to.have.property("root", root);
     });
+
+    it("should resolve relative root folder to `pwd`", function(){
+      var root = "./tmp",
+          resolvedRoot = path.join(__dirname, "..", "tmp"),
+          partition = new Partition({
+            root: root
+          });
+
+      expect(partition).to.have.property("root", resolvedRoot);
+    });
   });
 
-  // All of the following test should maintain the same behaviour as Paperclip's
-  // :id_partition interpolation as seen on:
-  // https://github.com/thoughtbot/paperclip/blob/v2.7.0/lib/paperclip/interpolations.rb#L152
-  //
-  // The following specs are used in Paperclip itself
-  //
-  // should "return the partitioned id of the attachment when the id is an integer" do
-  //   attachment = mock
-  //   attachment.expects(:id).returns(23)
-  //   attachment.expects(:instance).returns(attachment)
-  //   assert_equal "000/000/023", Paperclip::Interpolations.id_partition(attachment, :style)
-  // end
-
-  // should "return the partitioned id of the attachment when the id is a string" do
-  //   attachment = mock
-  //   attachment.expects(:id).returns("32fnj23oio2f")
-  //   attachment.expects(:instance).returns(attachment)
-  //   assert_equal "32f/nj2/3oi", Paperclip::Interpolations.id_partition(attachment, :style)
-  // end
-
-  // should "return nil for the partitioned id of an attachment to a new record (when the id is nil)" do
-  //   attachment = mock
-  //   attachment.expects(:id).returns(nil)
-  //   attachment.expects(:instance).returns(attachment)
-  //   assert_nil Paperclip::Interpolations.id_partition(attachment, :style)
-  // end
-
-  describe(".scheme()", function(){
+  describe("instance methods", function(){
     var partition,
-        root = path.join(__dirname, "tmp");
+        root = "tmp",
+        resolvedRoot = path.resolve(process.cwd(), root);
 
-    beforeEach(function(){
+    /** 
+     *
+     * All of the following test should maintain the same behaviour as Paperclip's
+     * :id_partition interpolation as seen on:
+     * https://github.com/thoughtbot/paperclip/blob/v2.7.0/lib/paperclip/interpolations.rb#L152
+     *
+     * The following specs are used in Paperclip itself
+     *
+     * should "return the partitioned id of the attachment when the id is an integer" do
+     *   attachment = mock
+     *   attachment.expects(:id).returns(23)
+     *   attachment.expects(:instance).returns(attachment)
+     *   assert_equal "000/000/023", Paperclip::Interpolations.id_partition(attachment, :style)
+     * end
 
-      partition = new Partition({root: root});
+     * should "return the partitioned id of the attachment when the id is a string" do
+     *   attachment = mock
+     *   attachment.expects(:id).returns("32fnj23oio2f")
+     *   attachment.expects(:instance).returns(attachment)
+     *   assert_equal "32f/nj2/3oi", Paperclip::Interpolations.id_partition(attachment, :style)
+     * end
+
+     * should "return nil for the partitioned id of an attachment to a new record (when the id is nil)" do
+     *   attachment = mock
+     *   attachment.expects(:id).returns(nil)
+     *   attachment.expects(:instance).returns(attachment)
+     *   assert_nil Paperclip::Interpolations.id_partition(attachment, :style)
+     * end
+     */ 
+    describe(".scheme()", function(){
+
+      beforeEach(function(){
+        partition = new Partition({root: root});
+      });
+
+      it("should return the partitioned id of the attachment when the id is an integer", function(){
+        expect(partition.scheme(23)).to.be("000/000/023");
+      });
+
+      it("should return the partitioned id of the attachment when the id is a string", function() {
+        expect(partition.scheme('32fnj23oio2f')).to.be("32f/nj2/3oi");
+      });
+
+      it("should return null for the partitioned id when the id is null", function() {
+        expect(partition.scheme(null)).to.be(null);
+      });
     });
 
-    it("should return the partitioned id of the attachment when the id is an integer", function(){
-      expect(partition.scheme(23)).to.be("000/000/023");
+    describe(".resolve()", function(){
+      beforeEach(function(){
+        partition = new Partition({root: root});
+      });
+
+      describe(".resolve(23)", function(){
+        it("should return absolute path", function(){
+          expect(partition.resolve(23)).to.be(path.join(partition.root, '23'));
+        });
+      });
+
+      describe(".resolve('000/000/023')", function(){
+        it("should return absolute path", function(){
+          expect(partition.resolve("000/000/023")).to.be(path.join(partition.root, "000", "000", "023"));
+        });
+      });
+
+      describe(".resolve('32fnj23oio2f')", function(){
+        it("should return absolute path", function(){
+          expect(partition.resolve("32fnj23oio2f")).to.be(path.join(partition.root, "32fnj23oio2f"));
+        });
+      });
+
+      describe(".resolve(null)", function(){
+        it("should return root path", function(){
+          expect(partition.resolve(null)).to.be(resolvedRoot);
+        });
+      });
     });
 
-    it("should return the partitioned id of the attachment when the id is a string", function() {
-      expect(partition.scheme('32fnj23oio2f')).to.be("32f/nj2/3oi");
+    describe(".prepare()", function(){
+
+      describe(".prepare(partition.scheme(23))", function(){
+        var target;
+
+        beforeEach(function(){
+          sh.rm("-Rf", resolvedRoot);
+          sh.mkdir("-p", resolvedRoot);
+          partition = new Partition({root: root});
+          target = partition.resolve(partition.scheme(23));
+        });
+
+        it("should call back without error", function(done) {
+          partition.prepare(target, done);
+        });
+
+        it("should create parent folder for scheme", function(done) {
+          partition.prepare(target, function(err) {
+            if(path.existsSync(path.dirname(target))) {
+              done();
+            }
+            else {
+              done(new Error("missing target parent folder " + path.dirname(target)));
+            }
+          });
+        });
+      });
     });
 
-    it("should return null for the partitioned id when the id is null", function() {
-      expect(partition.scheme(null)).to.be(null);
+    
+    describe(".link()", function(){
+
+      beforeEach(function(){
+        sh.rm("-Rf", resolvedRoot);
+        sh.mkdir("-p", resolvedRoot);
+        partition = new Partition({root: root});
+      });
+
+      describe("without src folder", function() {
+        it("should call back with error", function(done){
+          partition.link(23, function(err) {
+            if(err == "missing source folder '"+ partition.resolve(23) + "'") {
+              done();
+            }
+            else {
+              done(new Error("expected failure, got: '" + err + "'"));
+            }
+          });
+        });
+      });
+
+    
+      describe(".link(23, cb)", function(){
+        beforeEach(function(){
+          sh.mkdir("-p", partition.resolve(23));
+        });
+
+        describe("with existing target", function(){
+          beforeEach(function(){
+            sh.mkdir("-p", partition.resolve(partition.scheme(23)));
+          });
+
+          it("should call back with error", function(done){
+            partition.link(23, function(err) {
+              if(err.code == "EEXIST") {
+                done();
+              }
+              else {
+                done(new Error("expected failure, got: '" + err + "'"));
+              }
+            });
+          });
+        });
+
+        it("should call back without error", function(done){
+          partition.link(23, function(err) {
+            done(err);
+          }); 
+        });
+
+        it("should link to partitioned file", function(done){
+          var destination = partition.resolve(partition.scheme(23));
+          partition.link(23, function(err) {
+            if(path.existsSync(destination)) {
+              done();
+            }
+            else {
+              done(new Error("missing linked file" + destination));
+            }
+          });
+        });
+      });
     });
-
-
   });
 });
